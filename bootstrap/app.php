73257a81b7e38df0177b79833747096fc5736af4
shakir-ali110 <phpdevnarjis@gmail.com>
2026-06-02 15:47:42 +0530

    Configure trusted proxies and auto-enable secure cookies in production to fix 419 Page Expired errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'admin.auth' => \App\Http\Middleware\AdminAuth::class,
         ]);
